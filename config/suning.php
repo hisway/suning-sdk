f829c53f54fc2343faebb5b7bd856a8b8f05b1c1
hisway <hisway@qq.com>
2020-02-08 14:49:38 +0800

    suning sdk

--- config/suning.php
+++ config/suning.php
@@ -8,8 +8,8 @@ return [
 
     'connections' => [
         'app' => [
-            'app_key'       => env('SUNING_APP_KEY', 'APP KEY'),//5174bfbfceb17140e070019bebf96fc3
-            'app_secret'    => env('SUNING_APP_SECRET', 'APP SECRET'),//afaea374268c98f8b1de96682acbfbca
+            'app_key'       => env('SUNING_APP_KEY', 'APP KEY'),
+            'app_secret'    => env('SUNING_APP_SECRET', 'APP SECRET'),
             'server_url'    => env('SUNING_SERVER_URL', 'https://open.suning.com/api/http/sopRequest'),
             'format'        => 'json',
             'check_req'     => true,
